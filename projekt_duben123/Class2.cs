c543d3afb23bad09483aee38e9339df7c23cacdd
HP <HP@HP-PC>
2022-06-23 17:43:48 +0200

    Přidejte soubory projektu.

---
+++ projekt_duben123/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_duben123
+{
+    internal class Class2
+    {
+        public int ManaCost { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ projekt_duben123/Program.cs
@@ -0,0 +1,62 @@
+﻿global using projekt_duben123;
+
+
+Console.WriteLine("Generátor náhodných spellů");
+
+List<Class2> seznamSpellu = new List<Class2>();
+Class1 pomocneMetody = new Class1();
+Class2 Class2 = new Class2();
+
+string[] poleForma = pomocneMetody.PoleZTextu("txt/spellform.txt");
+string[] poleTypy = pomocneMetody.PoleZTextu("txt/spelltypes.txt");
+
+
+
+while (true)
+{
+    Console.WriteLine("1. vygenerovat nový spell a přidat do Spellbooku");
+    Console.WriteLine("2. zobrazit Spellbook");
+    Console.WriteLine("3. smazat poslední záznam ve Spellbooku");
+    Console.WriteLine("4. smazat celý Spellbook");
+    Console.WriteLine("*. ukončit aplikaci");
+
+    string input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        Class2 novySpell = new Class2();
+        novySpell.Name = pomocneMetody.VygenerujSpellname(poleForma, poleTypy);
+        novySpell.ManaCost = pomocneMetody.VygenerujCislo(100);
+
+        seznamSpellu.Add(novySpell);
+
+        Console.Clear();
+    }
+    else if (input == "2")
+    {
+        foreach (var spell in seznamSpellu)
+        {
+            Console.WriteLine($"Spell: {spell.Name}    Mana: {spell.ManaCost}");
+
+        }
+    }
+    else if (input == "3")
+    {
+        int indexPosledniho = seznamSpellu.Count - 1;
+
+        Class2 posledni = seznamSpellu[indexPosledniho];
+        seznamSpellu.Remove(posledni);
+
+        Console.Clear();
+    }
+    else if (input == "4")
+    {
+        seznamSpellu.Clear();
+
+        Console.Clear();
+    }
+    else if (input == "*")
+    {
+        break;
+    }
+}
